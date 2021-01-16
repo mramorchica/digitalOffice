f05d0bca583e38f9d82ae43874b20689e9aa4227
Emiliyan <emiliyan.kadiyski@shkolo.com>
2021-01-16 23:37:41 +0200

    Integrate AdminLTE admin layout

---
+++ resources/views/layouts/includes/head.blade.php
@@ -0,0 +1,10 @@
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="{{asset('adminlte/plugins/fontawesome-free/css/all.min.css')}}">
+<!-- Ekko Lightbox -->
+<link rel="stylesheet" href="{{asset('adminlte/plugins/ekko-lightbox/ekko-lightbox.css')}}">
+<!-- Theme style -->
+<link rel="stylesheet" href="{{asset('adminlte/css/adminlte.min.css')}}">
+<!-- overlayScrollbars -->
+<link rel="stylesheet" href="{{asset('adminlte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
---
+++ resources/views/test1.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.admin-lte')
+
+@section('content')
+<section class="content-header">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-6">
+        <h1>Kanban Board Title</h1>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- Main content -->
+<section class="content">
+
+  <!-- Default box -->
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">Kanban Board Title</h3>
+
+      <div class="card-tools">
+        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+          <i class="fas fa-minus"></i>
+        </button>
+      </div>
+    </div>
+    <div class="card-body p-0">
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+
+</section>
+    <!-- /.content -->
+@endsection
