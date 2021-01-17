4ba910f26cc2e283251038e317a658bb3f259d56
miro@inveitix.com <cvexxxxa@abv.bg>
2021-01-17 14:14:53 +0200

    vie single event (for logged in and not logged in users), and list of events(for both logged in and not logged in), and showing to non admin users, events based on is it draft, and is it public/private also if the time its not passed for the event

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,9 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('events/{event}','EventController@show')->name('events.show');
 
-Route::resource('events', 'EventController');
+Route::get('events/list/all','EventController@showList')->name('events.showList');
 
 Route::get('meet/{meetCode}','EventController@meet')->name('meet');
 
@@ -36,6 +37,9 @@ Route::middleware('auth')->group(function () {
 	Route::resource('users', 'Employees\UserController');
   
   Route::resource('news', 'NewsController');
-  Route::resource('news_categories', 'NewsCategoriesController')->except('show');	
+  Route::resource('news_categories', 'NewsCategoriesController')->except('show');
+
+  Route::resource('events', 'EventController')->except(['index','show']);
+  Route::get('events','EventController@index')->name('events.index')->middleware('checkUserRoleForEvents');
 });
 
